4dc79bbdb7839db2227c40fd7e33fb2bd992f8e5
IvoVillanueva <56939175+IvoVillanueva@users.noreply.github.com>
2025-11-10 17:15:31 +0100

    cargar test

---
+++ scripts/test_auth.R
@@ -0,0 +1,21 @@
+source("scripts/helpers.R")
+
+# Cargar variables del entorno
+token <- Sys.getenv("SM_TOKEN")
+url <- Sys.getenv("URL_JORNADA")
+
+# Comprobación básica
+if (token == "" || url == "") {
+  stop("❌ Faltan variables de entorno (SM_TOKEN o URL_JORNADA).")
+}
+
+# Petición de prueba
+res <- GET(url, add_headers(Authorization = token))
+
+# Verifica estado
+if (status_code(res) == 200) {
+  cat("✅ Autenticación correcta. Conexión establecida.\n")
+} else {
+  cat("❌ Error en la autenticación. Código:", status_code(res), "\n")
+  print(content(res, "text"))
+}
