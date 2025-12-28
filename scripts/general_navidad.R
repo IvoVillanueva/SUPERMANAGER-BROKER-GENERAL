03b4adbda609918d23cc6f4e45351263b6e24622
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2025-12-28 16:25:22 +0100

    Refactor Navidad report styling and colors
    Updated color palette and table styles for Navidad report.

--- scripts/general_navidad.R
+++ scripts/general_navidad.R
@@ -29,7 +29,7 @@ general %>%
   ) %>%
   head(20) %>%
   select(arrow, combo_img, username, nameTeam, totalPlayerPoints,
-    rk1 = position, brokerValor, rk2 = brokerPosition
+         rk1 = position, brokerValor, rk2 = brokerPosition
   ) %>%
   gt() %>%
   cols_align(
@@ -50,8 +50,8 @@ general %>%
     rk2 = gt::html("<span style='font-weight:bold;font-size:20px'>rk</span>"),
     brokerValor = gt::html("<span style='font-weight:bold;font-size:20px'>Broker</span>")
   ) %>%
-  gt_color_rows(rk1, palette = c("white", "#ffa031"), direction = -1) %>%
-  gt_color_rows(rk2, palette = c("white", "#ffa031"), direction = -1) %>%
+  gt_color_rows(rk1, palette = c("white", "#1F4D36"), direction = -1) %>%
+  gt_color_rows(rk2, palette = c("white", "#1F4D36"), direction = -1) %>%
   cols_width(
     c(combo_img) ~ px(56)
   ) %>%
@@ -71,23 +71,23 @@ general %>%
     locations = cells_body(columns = c(arrow)),
     fn = function(x) {
       rank_chg <- as.integer(x)
-
+      
       choose_logo <- function(x) {
         if (x == 0) {
-          gt::html(fontawesome::fa("equals", fill = "grey"))
+          gt::html(fontawesome::fa("equals", fill = "#DCDCDC"))
         } else if (x > 0) {
           gt::html(
-            glue::glue("<span style='color:#69b42e;font-face:bold;font-size:24px;'>{x}</span>"),
-            fontawesome::fa("arrow-up", fill = "#69b42e")
+            glue::glue("<span style='color:#1F4D36;font-face:bold;font-size:24px;'>{x}</span>"),
+            fontawesome::fa("arrow-up", fill = "#1F4D36")
           )
         } else if (x < 0) {
           gt::html(
-            glue::glue("<span style='color:#a12513;font-face:bold;font-size:24px;'>{x}</span>"),
-            fontawesome::fa("arrow-down", fill = "#a12513")
+            glue::glue("<span style='color:#C1121F;font-face:bold;font-size:24px;'>{x}</span>"),
+            fontawesome::fa("arrow-down", fill = "#C1121F")
           )
         }
       }
-
+      
       map(rank_chg, choose_logo)
     }
   ) %>%
@@ -95,7 +95,10 @@ general %>%
   tab_options(
     heading.align = "left",
     table.font.names = "Oswald",
-    table.background.color = "white",
+    table.background.color = "#E2CFA8",
+    table.border.top.color = "transparent",
+    table.border.bottom.color = "transparent",
+    row.striping.include_table_body = FALSE,
     table.font.size = 22,
     data_row.padding = px(1),
     source_notes.font.size = 15,
@@ -103,17 +106,21 @@ general %>%
   @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@400;600;700&display=swap');
   @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css');
   .gt_table, .gt_table * { font-family: 'Oswald', sans-serif !important; }
-  .gt_table { margin-bottom: 40px; }
+  .gt_table { margin-bottom: 40px; 
+             }
 "
-  ) |>
+  ) %>%
+  opt_css(
+    css = "body { background-color: #E2CFA8 !important; }"
+  ) %>%
   tab_header(
     title = md("<div style='line-height:134px;vertical-align:middle;text-align:left;font-weight:600;font-size:64px'>
                  <img src='https://raw.githubusercontent.com/IvoVillanueva/SUPERMANAGER-BROKER-GENERAL/refs/heads/main/png/LogoSMNavidadAzulmarino.png'style='width:114px; height:114px;vertical-align:middle;padding-right:12px'
-               <span style='text-align:left;'>Clasificación NAVIDAD</div>"),
-    subtitle = md(glue("<span style='font-weight:400;color:#8C8C8C;fon t-size:20px'>Lideres En la General TOP20 en el SUPERMANAGER DE NAVIDAD 25/26 hasta la jornada {jornada_navidad}</span>"))
+               <span style='text-align:left;'>CLASIFICACIÓN DE NAVIDAD</div>"),
+    subtitle = md(glue("<span style='font-weight:400;fon t-size:25px'>Lideres En la General TOP20 en el SUPERMANAGER DE NAVIDAD D25/26 hasta la jornada {jornada_navidad}</span>"))
   ) %>%
   tab_source_note(
     source_note = md(paste0("**Datos**: *@SuperManagerACB*&nbsp;&nbsp; <img src='https://raw.githubusercontent.com/IvoVillanueva/acb_logo/main/Logo%20SM%20mosca%20340x340.png'
                      style='height:25px;width:25px;vertical-align:middle;'>", caption))
-  ) %>%
-  gtsave(paste0("png/General_jor_navidad_", jornada_navidad, "_.png"), vwidth = 3000, vheight = 1500, expand = 200)
+  )%>%
+  gtsave(paste0("png/General_jor_navidad_", jornada_navidad, "_.png"), vwidth = 3000, vheight = 1500, expand = 100)
