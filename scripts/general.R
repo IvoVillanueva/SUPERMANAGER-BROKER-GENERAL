66f1deb0812afc7ed3e818ec99200be73a490237
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2025-12-28 12:26:31 +0100

    Update logo source in general.R

--- scripts/general.R
+++ scripts/general.R
@@ -107,7 +107,7 @@ general %>%
   ) |>
   tab_header(
     title = md("<div style='line-height:134px;vertical-align:middle;text-align:left;font-weight:600;font-size:64px'>
-                 <img src='https://raw.githubusercontent.com/IvoVillanueva/acb_logo/main/Logo%20SM%20mosca%20340x340.png'style='width:114px; height:114px;vertical-align:middle;padding-right:12px'
+                 <img src='https://raw.githubusercontent.com/IvoVillanueva/SUPERMANAGER-BROKER-GENERAL/refs/heads/main/png/LogoSMNavidadAzulmarino.png'style='width:114px; height:114px;vertical-align:middle;padding-right:12px'
                <span style='text-align:left;'>Clasificación General</div>"),
     subtitle = md(glue("<span style='font-weight:400;color:#8C8C8C;fon t-size:20px'>Lideres En la General TOP20 en el SuperManager 25/26 hasta la jornada {jornada}</span>"))
   ) %>%
