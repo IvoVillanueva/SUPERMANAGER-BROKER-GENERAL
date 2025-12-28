a281ef7eca830922c943c1ed1bd453263a35f098
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2025-12-28 12:42:58 +0100

    Modify script sources in main_supermanager_update.R
    Updated script sources for the supermanager update.

--- scripts/main_supermanager_update.R
+++ scripts/main_supermanager_update.R
@@ -1,3 +1,5 @@
 source("scripts/general.R")
-source("scripts/brokergeneral.R")
-source("scripts/02_ranking_jornada.R")
+source("scripts/general_navidad.R")
+#source("scripts/brokergeneral.R")
+#source("scripts/02_ranking_jornada.R")
+
