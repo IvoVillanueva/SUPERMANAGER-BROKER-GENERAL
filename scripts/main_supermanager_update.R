908fe5a050f8f17a9e6f6666a179dc5117cada1f
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2026-05-28 07:36:19 +0200

    Update main_supermanager_update.R

--- scripts/main_supermanager_update.R
+++ scripts/main_supermanager_update.R
@@ -1,5 +1,5 @@
 source("scripts/general.R")
 #source("scripts/general_navidad.R")
 source("scripts/brokergeneral.R")
-source("scripts/02_ranking_jornada.R")
+#source("scripts/02_ranking_jornada.R")
 
